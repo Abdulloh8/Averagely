1797ca366b53e939fe2ea577376f5fa200a41c99
Your Name <you@example.com>
2023-07-25 13:58:34 +0500

    Switch Operation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            Console.WriteLine("tick the number of correct answers from 0 to 30");
+            Console.WriteLine("tick the number of correct answers from 0 to 5");
 
             Console.Write("Math : ");
             int math = Convert.ToInt32(Console.ReadLine());
@@ -21,49 +21,22 @@
             Console.Write("algebra : ");
             int algebra = Convert.ToInt32(Console.ReadLine());
 
-            if(math > 30){
-                math = 30;
-            }
-            else if (math < 0)
-            {
-                math = 0;
-            }
-            if(physics > 30){
-                physics = 30;
-            }
-            else if (physics < 0)
-            {
-                physics = 0;
-            }
-            if(english > 30){
-                english = 30;
-            }
-            else if (english < 0)
-            {
-                english = 0;
-            }
-            if(chemistry > 30){
-                chemistry = 30;
-            }
-            else if (chemistry < 0)
-            {
-                chemistry = 0;
-            }
-            if(algebra > 30){
-                algebra = 30;
-            }
-            else if (algebra < 0)
-            {
-                algebra = 0;
-            }
+            int ball = (math + physics + english + chemistry + algebra) / 5;
+            //Console.WriteLine($"your total score {ball / 5}");
 
+            string resualt = ball switch
+            {
+                1 => "your resualt very bad",
+                2 => "your resualt very bad",
+                3 => "your resualt  bad",
+                4 => "your resualt good",
+                5 => "your resualt very good",
+                _ => "Error resualt"
+            };
 
-            int ball = math + physics + english + chemistry + algebra ;
-            Console.WriteLine($"your total score {ball / 5}");
+            Console.WriteLine(resualt); 
         }
-
     }
-
 }
 
 
