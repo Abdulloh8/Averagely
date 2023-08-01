1bdff1c332de51db87581cec1937dc4927b227a7
Your Name <you@example.com>
2023-08-01 14:10:47 +0500

    Show Errors

--- Program.cs
+++ Program.cs
@@ -4,37 +4,66 @@
     {
         static void Main()
         {
-            Console.WriteLine("tick the number of correct answers from 0 to 5");
-
-            Console.Write("Math : ");
-            int math = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("physics : ");
-            int physics = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("English language : ");
-            int english = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("chemistry : ");
-            int chemistry = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("algebra : ");
-            int algebra = Convert.ToInt32(Console.ReadLine());
-
-            int ball = (math + physics + english + chemistry + algebra) / 5;
-            //Console.WriteLine($"your total score {ball / 5}");
-
-            string resualt = ball switch
+            try
             {
-                1 => "your resualt very bad",
-                2 => "your resualt very bad",
-                3 => "your resualt  bad",
-                4 => "your resualt good",
-                5 => "your resualt very good",
-                _ => "Error resualt"
-            };
+                while(true)
+                {
+                    Console.WriteLine("tick the number of correct answers from 0 to 5");
 
-            Console.WriteLine(resualt); 
+                    Console.Write("Math : ");
+                    int math = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("physics : ");
+                    int physics = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("English language : ");
+                    int english = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("chemistry : ");
+                    int chemistry = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("algebra : ");
+                    int algebra = Convert.ToInt32(Console.ReadLine());
+
+                    int ball = (math + physics + english + chemistry + algebra) / 5;
+                    
+
+                    string resualt = ball switch
+                    {
+                        1 => "your resualt very bad",
+                        2 => "your resualt very bad",
+                        3 => "your resualt  bad",
+                        4 => "your resualt good",
+                        5 => "your resualt very good",
+                        _ => "Error resualt"
+                   };
+
+                    Console.WriteLine(resualt);
+                    Console.WriteLine("Enter any character to continue");
+                    Console.WriteLine("enter 1 to finish");
+                    string finish = Console.ReadLine();
+                    if(finish == "1")
+                    {
+                        break;
+                    }
+
+                }
+                
+
+            }
+            catch(FormatException FormatException)
+            {
+                Console.WriteLine("this value is invalid");
+            }
+            catch(OverflowException OverflowException)
+            {
+                Console.WriteLine("this value does not match ");
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine("there is an error in the program");
+            }
+            
         }
     }
 }
